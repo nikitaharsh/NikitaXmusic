f99edea7ca9d3cf51e11de0b31d8feda5b450f0e
nikitaharsh <140864546+nikitaharsh@users.noreply.github.com>
2023-08-01 22:48:21 +0530

    Update stop.py

--- NikitaX/plugins/stop.py
+++ NikitaX/plugins/stop.py
@@ -3,11 +3,11 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from nikitaX import app
+from NikitaX import app
 from NikitaX.core.call import Anon
 from NikitaX.utils.database import set_loop
-from nikitaX.utils.decorators import AdminRightsCheck
-from nikitaX.utils.inline.play import close_keyboard
+from NikitaX.utils.decorators import AdminRightsCheck
+from NikitaX.utils.inline.play import close_keyboard
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
