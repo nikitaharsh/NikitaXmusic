1ab6e951798de9d4cdc9112f7db27dc9058ab9c9
nikitaharsh <140864546+nikitaharsh@users.noreply.github.com>
2023-08-01 21:59:03 +0530

    Update active.py

--- NikitaX/plugins/active.py
+++ NikitaX/plugins/active.py
@@ -3,8 +3,8 @@ from pyrogram.types import Message
 
 from strings import get_command
 from NikitaX import app
-from nikitaX.misc import SUDOERS
-from nikitaX.utils.database.memorydatabase import (
+from NikitaX.misc import SUDOERS
+from NikitaX.utils.database.memorydatabase import (
     get_active_chats, get_active_video_chats)
 
 # Commands
